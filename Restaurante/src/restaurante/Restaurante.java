32d232815dbb46b91bb5e687bb995cac5d629358
RicardoNogueiraReis <ricardovreis499@gmail.com>
2021-05-09 16:15:47 +0100

    Ficheiros iniciais do NetBeans

---
+++ Restaurante/src/restaurante/Restaurante.java
@@ -0,0 +1,16 @@
+package restaurante;
+
+/**
+ *
+ * @author Ricardo Reis 200262024
+ */
+public class Restaurante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
